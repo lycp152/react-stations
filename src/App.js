2825d7e21f75c35bd06abb737eee2d5fa4d94f4f
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-07-07 23:32:52 +0900

    station2まで

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h1>your WANKO</h1>
+      </header>
+      <h2>犬の画像を表示するサイトです</h2>
+      <img src="https://dog.ceo/dog-api/"></img>
     </div>
   )
 }
