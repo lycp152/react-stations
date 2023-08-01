33355f138a7a8ece0319d4249a549a5b312b5f6f
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-01 22:37:52 +0900

    station9まで

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,24 @@
 // DO NOT DELETE
+import * as React from 'react'
+import { useState, useEffect } from 'react';
+
+export const DogListContainer = () => {
+  const [breeds, setBreeds] = useState('https://dog.ceo/api/breeds/list/all');
+
+  breeds
+
+  useEffect(() => {
+    const handleUpdate = () => {
+      fetch('https://dog.ceo/api/breeds/list/all')
+        .then(response => response.json())
+        .then(data => setBreeds(data.message))
+        .catch(error => console.log(error));
+    };
+    handleUpdate()
+  }, []  )
+
+  return (
+    <div>
+    </div>
+  );
+};
