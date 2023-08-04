b1336437c7d1b99db8e80390a7cb43cc16804997
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-08-04 16:16:31 +0900

    クリア！

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,34 +1,50 @@
 // DO NOT DELETE
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { DogImageList } from './DogImageList';
 
 const BreedsSelect = ({ breeds }) => {
-  const [selectedBreed, setSelectedBreed] = React.useState('');
+  const [selectedBreed, setSelectedBreed] = React.useState(breeds[0]);
+  const [dogImages, setDogImages] = React.useState([]);
+  const [showImages, setShowImages] = React.useState(true); // 初期状態をtrueに設定
 
-  const handleChange = (event) => {
-    setSelectedBreed(event.target.value);
+  const handleUpdate = async () => {
+    try {
+      const response = await fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/12`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setDogImages(data.message);
+      setShowImages(true);
+    } catch (error) {
+      console.error('Error fetching dog images:', error);
+    }
   };
 
   return (
-    <div id="dropdown">
-      <p>Breeds List</p>
-      {/* breedsプロップスを元に犬種のドロップダウンを作成 */}
-      <select value={selectedBreed} onChange={handleChange}>
-        {/* breedsが配列の場合、犬種の選択肢を生成 */}
-        {Array.isArray(breeds) &&
-          breeds.map((breed, index) => (
-            <option key={index} value={breed}>
-              {breed}
-            </option>
-          ))}
-      </select>
+    <div>
+      <div id="dropdown">
+        <p>Breeds List</p>
+        <select value={selectedBreed} onChange={(event) => setSelectedBreed(event.target.value)}>
+          {Array.isArray(breeds) &&
+            breeds.map((breed, index) => (
+              <option key={index} value={breed}>
+                {breed}
+              </option>
+            ))}
+        </select>
+        <button type="button" onClick={handleUpdate} id="displayButton">
+          表示
+        </button>
+      </div>
+      {showImages && <DogImageList dogImages={dogImages} />}
     </div>
   );
 };
 
-// プロップスの検証を追加
 BreedsSelect.propTypes = {
   breeds: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export { BreedsSelect }; // ファイルの最後に直接export
+export { BreedsSelect };
